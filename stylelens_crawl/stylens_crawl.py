7c45cbc7ea9bd80151934ca36ad507e6be434d34
bluehack-jhlim <jh.lim@bluehack.net>
2017-12-06 11:04:54 +0900

    Bug fixed: The basedir changed to temporary directory.

--- stylelens_crawl/stylens_crawl.py
+++ stylelens_crawl/stylens_crawl.py
@@ -1,13 +1,15 @@
 import os
 import json
 import logging
+import tempfile
+import shutil
 
 from scrapy.crawler import CrawlerProcess
 from stylelens_crawl.services.DeBow import DeBow
 from stylelens_crawl.services.DoubleSixGirls import DoubleSixGirls
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = tempfile.mkdtemp()
 
 class StylensCrawler(object):
     def __init__(self, options):
@@ -19,12 +21,10 @@ class StylensCrawler(object):
             'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
             'FEED_FORMAT': 'json',
             'FEED_EXPORT_ENCODING': 'UTF-8',
-            'FEED_URI': 'out.json',
+            'FEED_URI': os.path.join(BASE_DIR, 'out.json'),
             'LOG_LEVEL': 'INFO'
         })
-
-        if os.path.exists(os.path.join(BASE_DIR, 'out.json')):
-            os.remove(os.path.join(BASE_DIR, 'out.json'))
+        self.logger.info('The file location: %s' % os.path.join(BASE_DIR, 'out.json'))
 
     def start(self):
 
@@ -48,3 +48,14 @@ class StylensCrawler(object):
             return raw_data
         else:
             raise RuntimeError('The result file dose not exist.')
+
+    @staticmethod
+    def delete_temp_file():
+        '''
+        Mostly you don't need to call this function,
+        if you use micro service architecture.
+        :return:
+        '''
+        if os.path.exists(BASE_DIR):
+            return shutil.rmtree(BASE_DIR)
+        return False
